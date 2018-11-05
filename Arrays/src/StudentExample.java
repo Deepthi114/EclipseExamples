1bc75552de613944d8a11776162c8f359fb3c2bd
Deepu <Deepthi@gmail.com>
2018-11-05 18:21:33 -0500

    Arrays

---
+++ Arrays/src/StudentExample.java
@@ -0,0 +1,23 @@
+
+public class StudentExample {
+	
+
+	public static void main(String[] args) {
+		
+		
+		int a[]=new int[10];
+		
+		for(int b=0;b<=a.length;b++)
+		{
+			a[b]=b+1;
+		}
+		for(int b=0;b<=a.length;b++)
+	
+			System.out.println("Student Roll Numbers :"+a[b]);
+	
+
+		
+
+	}
+
+}
